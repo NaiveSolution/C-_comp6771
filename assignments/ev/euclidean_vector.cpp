2dff671a15431e63be73b5c7f4bd7dfeeafeee38
Tariq Mosaval <z5214836@unsw.edu.au>
2019-07-02 10:04:16 +1000

    added cpp and h files

--- assignments/ev/euclidean_vector.cpp
+++ assignments/ev/euclidean_vector.cpp
@@ -1,3 +1,17 @@
 #include "assignments/ev/euclidean_vector.h"
 
 #include <algorithm>  // Look at these - they are helpful https://en.cppreference.com/w/cpp/algorithm
+
+EuclideanVector::EuclideanVector(int x, double y) {
+  this->num_dimensions_ = x;
+  this->magnitudes_ = std::make_unique<double[]>(x);
+
+  for (int i = 0; i != x; ++i){
+    magnitudes_[i] = y;
+  }
+}
+
+EuclideanVector::EuclideanVector(std::vector<double>::const_iterator front_it,
+                                 std::vector<double>::const_iterator back_it) {
+
+}
--- assignments/ev/euclidean_vector.h
+++ assignments/ev/euclidean_vector.h
@@ -3,21 +3,45 @@
 #include <exception>
 #include <string>
 #include <memory>
+#include <vector>
 
 class EuclideanVectorError : public std::exception {
+
  public:
   explicit EuclideanVectorError(const std::string& what) : what_(what) {}
   const char* what() const noexcept{ return what_.c_str(); }
+
  private:
   std::string what_;
 };
 
 class EuclideanVector {
+
  public:
-  explicit EuclideanVector(int i);
+  // constructors
+  explicit EuclideanVector(int) : EuclideanVector(1, 0.0){}
+  EuclideanVector(int x, double y);
+  EuclideanVector(std::vector<double>::const_iterator, std::vector<double>::const_iterator );
+
+  // overloads
   friend std::ostream& operator<<(std::ostream& os, const EuclideanVector& v);
-  // TODO(you): add more
+  friend EuclideanVector operator+(const EuclideanVector& lhs, const EuclideanVector& rhs);
+
+  // methods
+  int GetNumDimensions();
+
  private:
+  int num_dimensions_;
   std::unique_ptr<double[]> magnitudes_;
-  // TODO(you): add more
+
+
 };
+/*
+
+EuclideanVector operator+(const EuclideanVector& lhs, const EuclideanVector& rhs){
+
+}
+
+int EuclideanVector::GetNumDimensions() {
+
+}*/
