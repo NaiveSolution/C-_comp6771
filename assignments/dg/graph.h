ddad4799bef7a29349650f937341d68ed38f527c
Jimmy <z5163448@cse.unsw.edu.au>
2019-07-19 18:43:36 +1000

    added private attributes

--- assignments/dg/graph.h
+++ assignments/dg/graph.h
@@ -1,21 +1,46 @@
 #ifndef ASSIGNMENTS_DG_GRAPH_H_
 #define ASSIGNMENTS_DG_GRAPH_H_
 
+#include <memory>
+#include <vector>
+
 namespace gdwg {
 
 template<typename N, typename E>
 class Graph {
  public:
  private:
-};
+  struct Node;
+  struct Edge;
 
+  /* Node data structure that stores a generic value,
+   * incoming and outgoing edges, in and out degrees
+   */
+  struct Node {
+    N value_;
+    std::vector<Edge> inedge_;
+    std::vector<Edge> outedge_;
+    int indegree_;
+    int outdegree_;
+  };
+
+  /* Edge data structure that stores a generic weight,
+   * source and destination nodes.
+   */
+  struct Edge {
+    E weight_;
+    Node src_;
+    Node dest_;
+  };
+
+  std::vector<std::unique_ptr<Node>> nodes_;
+  std::vector<std::unique_ptr<Edge>> edges_;
+};
 // Define your graph_iterator here
 // Define your graph_const_iterator here
 
 }
 
 #include "assignments/dg/graph.tpp"
-
-#hi
 
 #endif  // ASSIGNMENTS_DG_GRAPH_H_
